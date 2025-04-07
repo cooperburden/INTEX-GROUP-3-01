4bf96abc344bdaba9d98a1f1ee1d9516ed05f7c8
cooperburden <131023054+cooperburden@users.noreply.github.com>
2025-04-07 16:19:45 -0600

    Update MovieList.tsx
    copy and paste

--- frontend/src/components/MovieList.tsx
+++ frontend/src/components/MovieList.tsx
@@ -1,54 +1,54 @@
 import { useEffect, useState } from "react";
-import { Movie } from "../types/Movie";
-import GenreFilter from "../components/GenreFilter";
+import { Movie } from "./types/Movie";
+import GenreFilter from "./components/GenreFilter"
 
 function MovieList() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [genres, setGenres] = useState<string[]>([]);
   const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [pageSize, setPageSize] = useState(10);
+  const [pageSize, setPageSize] = useState(10)
+
 
   useEffect(() => {
-    fetch(
-      "http://localhost:5009/api/MovieTitles?pageSize=5&pageNum=1&sortOrder=asc"
-    )
+    fetch("http://localhost:5009/api/MovieTitles")
       .then((res) => res.json())
       .then((data) => {
-        setMovies(data.movies);
-
-        if (data.movies.length > 0) {
-          const firstMovie = data.movies[0];
-          const genreKeys = Object.keys(firstMovie).filter(
-            (key) =>
-              typeof firstMovie[key] === "number" &&
-              (firstMovie[key] === 0 || firstMovie[key] === 1)
+        setMovies(data);
+  
+        if (data.length > 0) {
+          const firstMovie = data[0];
+          const genreKeys = Object.keys(firstMovie).filter((key) =>
+            typeof firstMovie[key] === "number" &&
+            (firstMovie[key] === 0 || firstMovie[key] === 1)
           );
           setGenres(genreKeys);
         }
       })
       .catch((err) => console.error("Error fetching movies:", err));
   }, []);
-
+  
   const handleGenreChange = (genres: string[]) => {
     setSelectedGenres(genres);
   };
 
-  const filteredMovies =
-    selectedGenres.length === 0
-      ? movies
-      : movies.filter((movie) =>
-          selectedGenres.every((genre) => movie[genre as keyof Movie] === 1)
-        );
+  const filteredMovies = selectedGenres.length === 0
+  ? movies
+  : movies.filter((movie) =>
+      selectedGenres.every((genre) => movie[genre as keyof Movie] === 1)
+    );
+
+
+
 
   return (
     <div style={{ display: "flex" }}>
       <GenreFilter
-        genres={genres}
-        selectedGenres={selectedGenres}
-        onGenreChange={handleGenreChange}
-      />
-
+            genres={genres}
+            selectedGenres={selectedGenres}
+            onGenreChange={handleGenreChange}
+    />
+  
       <div style={{ flexGrow: 1, padding: "1rem 2rem", marginLeft: "4rem" }}>
         <h1>Movies</h1>
         <div style={{ display: "flex", flexWrap: "wrap", gap: "1rem" }}>
@@ -68,9 +68,12 @@ function MovieList() {
             </div>
           ))}
         </div>
+                  
+
       </div>
     </div>
   );
+  
 }
 
 export default MovieList;
