2d5f81b86fc360d11e53b3dde9c3b38a43413f24
annay <akyoung@byu.edu>
2025-04-07 16:46:49 -0600

    Fixed urls

--- backend/MoviesIntex/MoviesIntex/Program.cs
+++ backend/MoviesIntex/MoviesIntex/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy  =>
         {
-            policy.WithOrigins("http://localhost:5175")
+            policy.AllowAnyOrigin()
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
