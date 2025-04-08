5e77610e23d0479fd54d4a2e102566e7609c67ae
cooperburden <cooperburden@gmail.com>
2025-04-08 15:14:07 -0600

    updated home page

---
+++ frontend/src/components/HomeHeader.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "../styles/Header.scss";
+
+function Header() {
+  return (
+    <>
+      <header className="header">
+        {/* Logo on the left */}
+        <img
+          src="../../public/logo.png"
+          alt="Company Logo"
+          style={{ width: "125px", height: "125px" }}
+          className="logo"
+        />
+
+        <div className="wrapper-right">
+
+          {/* User Icon on the right */}
+          <div className="dropdown">
+            <img
+              src="../../public/user-icon-white.svg"
+              alt="User Icon"
+              style={{ width: "50px", height: "60px" }}
+            />
+            {/* Triangle trigger for dropdown */}
+            <img
+              src="../../public/trianglewhite.svg"
+              alt="Dropdown Arrow"
+              className="dropdown-icon"
+              style={{ width: "15px", height: "15px" }}
+            />
+
+            {/* Dropdown menu */}
+            <div className="dropdown-menu">
+              <ul>
+                <li>Profile</li>
+                <li>Settings</li>
+                <li>Logout</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
---
+++ frontend/src/components/HomeVideoPlayer.tsx
@@ -0,0 +1,107 @@
+const VideoPlayer = () => {
+    return (
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "90vh", // Video takes top 90% (matches your layout)
+          overflow: "hidden",
+          zIndex: 0, // Below cards
+          background: "#000",
+        }}
+      >
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          disablePictureInPicture
+          controls={false}
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            pointerEvents: "none",
+            display: "block",
+          }}
+        >
+          <source src="/video/vid.mp4" type="video/mp4" />
+          Video not supported
+        </video>
+  
+        {/* Grey overlay from VideoHero */}
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.4)", // Grey overlay
+            zIndex: 1, // Above video, below text
+          }}
+        />
+  
+        {/* Text + Button from VideoHero */}
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            zIndex: 2, // Above overlay
+            textAlign: "center",
+            color: "white",
+          }}
+        >
+          <h1
+            style={{
+              fontSize: "4rem",
+              fontWeight: "800",
+              marginBottom: "1.5rem",
+              letterSpacing: "-0.5px",
+              lineHeight: "1.1",
+            }}
+          >
+            Lights. Camera. Stream.
+          </h1>
+          <button
+            style={{
+              padding: "0.75rem 1.5rem",
+              fontSize: "1.2rem",
+              backgroundColor: "red",
+              border: "none",
+              borderRadius: "5px",
+              color: "white",
+              cursor: "pointer",
+            }}
+            onClick={() =>
+              window.scrollTo({ top: window.innerHeight, behavior: "smooth" })
+            }
+          >
+            Create Account
+          </button>
+        </div>
+  
+        {/* Gradient overlay for greyed-out effect from VideoPlayer */}
+        <div
+          style={{
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            width: "100%",
+            height: "200px", // Height of fade (adjustable)
+            background:
+              "linear-gradient(to top, rgba(128, 128, 128, 0.8), transparent)",
+            pointerEvents: "none",
+            zIndex: 1, // Same level as grey overlay
+          }}
+        />
+      </div>
+    );
+  };
+  
+  export default VideoPlayer;
+  
