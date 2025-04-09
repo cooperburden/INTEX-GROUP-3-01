690508163c8874f7e99a69bee8255f576eb462a1
cooperburden <cooperburden@gmail.com>
2025-04-09 14:50:55 -0600

    commiting changes first

--- backend/MoviesIntex/MoviesIntex/obj/Debug/net8.0/MoviesIntex.AssemblyInfo.cs
+++ backend/MoviesIntex/MoviesIntex/obj/Debug/net8.0/MoviesIntex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesIntex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+224888d7f19441c8aa3155001cf952054b9f909e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0b80f030f4f631e9ff6543b2c1aca648e11c956")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesIntex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesIntex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
