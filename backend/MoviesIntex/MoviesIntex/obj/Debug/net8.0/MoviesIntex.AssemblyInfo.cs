46ab141de0f6e4bdaffedd1093e537c7d64a7cd5
annay <akyoung@byu.edu>
2025-04-09 12:45:28 -0600

    Style

--- backend/MoviesIntex/MoviesIntex/obj/Debug/net8.0/MoviesIntex.AssemblyInfo.cs
+++ backend/MoviesIntex/MoviesIntex/obj/Debug/net8.0/MoviesIntex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesIntex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+382cc4750c36281e40dfcc813d6f8bcedea09d73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+664987a8596705add9702412144ca39510ac60e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesIntex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesIntex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
