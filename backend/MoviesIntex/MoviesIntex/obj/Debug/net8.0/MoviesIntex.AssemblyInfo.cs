8b8a04104cbf8d79265530105f3a0d97d80eebeb
sadie21393 <sadie21@byu.edu>
2025-04-10 11:14:25 -0600

    merge

--- backend/MoviesIntex/MoviesIntex/obj/Debug/net8.0/MoviesIntex.AssemblyInfo.cs
+++ backend/MoviesIntex/MoviesIntex/obj/Debug/net8.0/MoviesIntex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesIntex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8803d33af6a4badf26ecdae937df9b458c6dcc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ad114269777255011f581cfe355105168c461b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesIntex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesIntex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
