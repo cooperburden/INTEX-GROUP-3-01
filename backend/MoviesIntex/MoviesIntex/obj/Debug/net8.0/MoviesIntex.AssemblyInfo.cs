551dc3bb5aa3e9079fa7f81c6866a6c5391d86c8
sadie21393 <sadie21@byu.edu>
2025-04-10 22:48:57 -0600

    working

--- backend/MoviesIntex/MoviesIntex/obj/Debug/net8.0/MoviesIntex.AssemblyInfo.cs
+++ backend/MoviesIntex/MoviesIntex/obj/Debug/net8.0/MoviesIntex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesIntex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25d3ad8d389add86e51661548fc6543f219a0f7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f36fb5c1f89f1d500507c8de210dd06b07dcf418")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesIntex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesIntex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
